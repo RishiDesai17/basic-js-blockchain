73b2972377dad9715e166f7131a834b23c3fc102
RishiDesai17 <rishindesai@yahoo.com>
2020-03-01 20:52:27 +0530

    get route

--- routes/blockchain.js
+++ routes/blockchain.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const BlockChain = require('../blockchain/blockChain');
 const Blockchain = require('../models/blockchainModel');
 
+router.get("/",async(req,res)=>{
+    const response = await Blockchain.find();
+    res.json({docs: response})
+})
+
 router.post("/",async(req,res)=>{
     let blockChain = new BlockChain();
     blockChain.addNewTransaction(req.body.from,req.body.to,req.body.money);
